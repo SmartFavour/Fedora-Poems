6d7de150b29823ff29be45208d2574e085caf6a3
SmartFavour <astech4245@gmail.com>
2026-05-12 11:17:48 +0000

    more poems added

--- Poetry/fedopoems/src/Home.js
+++ Poetry/fedopoems/src/Home.js
@@ -6,6 +6,10 @@ function Home() {
     <div className="Home">
       <SearchBar />
       <PoemCard title="Girls meet boys" content="This is a test module." />
+      <PoemCard
+        title="Twilight"
+        content="As a dream far to reach, stars shine in the morning."
+      />
     </div>
   );
 }
